eece1d4d26bd9e518b9d4de09fb5185bc6cd0712
svlandeg <svlandeg@github.com>
2026-03-30 16:00:33 +0000

    exp: DEVICE_BATCH_SIZE=32 (grad_accum 2->1, less overhead)

--- train.py
+++ train.py
@@ -448,7 +448,7 @@ FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
 
 # Model size
 DEPTH = 8               # number of transformer layers
-DEVICE_BATCH_SIZE = 16  # per-device batch size (reduce if OOM)
+DEVICE_BATCH_SIZE = 32  # per-device batch size (grad_accum=1 with TOTAL=2^16)
 
 # ---------------------------------------------------------------------------
 # Setup: tokenizer, model, optimizer, dataloader
