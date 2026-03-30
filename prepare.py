ad526629c8fc405a7af8ac1dd6beacd9f7db8004
svlandeg <svlandeg@github.com>
2026-03-30 16:08:24 +0200

    reduce eval tokens to speed up eval

--- prepare.py
+++ prepare.py
@@ -29,7 +29,7 @@ import torch
 
 MAX_SEQ_LEN = 2048       # context length
 TIME_BUDGET = 300        # training time budget in seconds (5 minutes)
-EVAL_TOKENS = 40 * 524288  # number of tokens for val eval
+EVAL_TOKENS = 4 * 524288  # number of tokens for val eval
 
 # ---------------------------------------------------------------------------
 # Configuration
